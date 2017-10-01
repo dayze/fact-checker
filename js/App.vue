9032b2f969d846b4f08fe0b2a54f0e2adab27cd8
Jeremy Habit <jeremy.habit@gmail.com>
2017-10-02 00:56:06 +0200

    filters bar, first query view & others details

--- js/App.vue
+++ js/App.vue
@@ -1,56 +1,89 @@
 <template>
-    <div id="app">
+  <div id="app">
 
-        <div id="footerPusher">
+    <div id="footerPusher">
 
-            <!-- @@@ HEADER @@@ -->
-            <header class="row flex-x-center padding-50-top-bottom padding-15-s padding-50-bottom-s">
-                <div class="cell-8 c-10-xl c-12-l align-center">
-                    <h1 class="color-yang">
-                        <a class="txt-size-jumbo APP_font_bebase" href="" title="home">Facts Checker</a>
-                        <span class="block txt-unstyled APP_font_vincentia">
+      <!-- @@@ HEADER @@@ -->
+      <header class="row flex-x-center padding-50-top-bottom padding-15-s padding-50-bottom-s">
+        <div class="cell-8 c-10-xl c-12-l align-center">
+          <h1 class="color-yang">
+            <a class="txt-size-jumbo APP_font_bebase" href="" title="home">Facts Checker</a>
+            <span class="block txt-unstyled APP_font_vincentia">
                         <span class="color-emerald">truth</span> & <span class="color-alizarin">hoax</span>
                     </span>
-                    </h1>
-                    <input v-model="query"
-                           class="width-60 width-100-xs padding border-none shaded-box margin-15-top-xs"
-                           id="inputSearch"
-                           name="inputSearch"
-                           type="search" title="Type something ..." placeholder="Trump Sharks Miami ..." autofocus/>
-                    <button v-on:keyup.enter="key" @click="search"
-                            id="searchBtn" title="Launch the research"
-                            class="margin-30-top-xs border-none color-yang transition-3 bg-turquoise hover-bg-concrete padding-5 padding-15-right-left">
-                        <span class="hide-m show-xs">Search</span>
-                        <span class="hide show-m hide-xs">GO</span>
-                    </button>
-                </div>
-            </header>
+          </h1>
+          <input v-model="query"
+                 class="width-60 width-50-m width-100-s padding border-none shaded-box margin-15-top-xs"
+                 id="inputSearch" name="inputSearch" type="search" title="Type something ..."
+                 placeholder="Trump Sharks Miami ..." autofocus/>
+          <button v-on:keyup.enter="key" @click="search"
+                  id="searchBtn" title="Launch the research"
+                  class="margin-30-top-s border-none color-yang transition-3 bg-turquoise hover-bg-concrete padding-5 padding-15-right-left">
+            <span class="hide-m show-xs">Search</span>
+            <span class="hide show-m hide-xs">Go</span>
+          </button>
+
+
+          <!--   <input v-model="query"
+                    class="width-60 width-100-xs padding border-none shaded-box margin-15-top-xs"
+                    id="inputSearch"
+                    name="inputSearch"
+                    type="search" title="Type something ..." placeholder="Trump Sharks Miami ..." autofocus/>
+             <button v-on:keyup.enter="key" @click="search"
+                     id="searchBtn" title="Launch the research"
+                     class="margin-30-top-xs border-none color-yang transition-3 bg-turquoise hover-bg-concrete padding-5 padding-15-right-left">
+               <span class="hide-m show-xs">Search</span>
+               <span class="hide show-m hide-xs">GO</span>
+             </button>-->
+        </div>
+      </header>
 
       <!-- @@@ FILTER BAR @@@ -->
-      <section v-if="Object.keys(facts).length !== 0" class="row border-bottom border-silver flex-x-center">
-        <div class="cell-12-margin-0 align-center">
-          <ul class="list-unstyled list-inline inline block list-separator">
-            <li class="padding-top-bottom">
-              <span>Filter</span>
-            </li>
-            <li class="padding-top-bottom">
+      <button class="accordion txt-size-medium">
+        <span>&#9660;</span>
+        <span>Filters</span>
+      </button>
+      <div class="panel">
+        <section class="row-column-m border-silver border-bottom flex-x-center align-center">
+          <div class="cell-margin-0 border-right border-silver">
+            <ul class="list-unstyled list-inline inline-block list-block-xl">
+              <li class="padding-top-bottom txt-b color-green-sea">
+                Research filters
+              </li>
+              <li class="padding-top-bottom">
+                <span class="margin-right-left">between</span>
+                <input id="inputDateRangeFrom" name="inputDateRangeFrom" type="date"/>
+              </li>
+              <li class="margin-15-bottom-xl">
+                <span class="margin-right-left">and</span>
+                <input id="inputDateRangeTo" name="inputDateRangeTo" type="date"/>
+              </li>
+            </ul>
+          </div>
+          <div v-if="Object.keys(facts).length !== 0" class="cell-6-margin-0 border-top-m border-silver">
+            <ul class="list-unstyled list-inline inline-block list-block-xl">
+              <li class="padding-top-bottom txt-b color-green-sea">
+                Result filters
+              </li>
+              <li class="padding-top-bottom">
                 <input class="dateCheck" id="inputDate" name="inputDate" type="checkbox" v-model="dateOrder"
                        @click="orderDate">
-              <label for="inputDate" class="color-turquoise">date</label>
-            </li>
-            <li class="vertical-middle">
-              <div class="onoffswitch">
-                <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked
-                    @click="orderTruthiness" v-model="truthinessOrder">
-                <label class="onoffswitch-label" for="myonoffswitch">
-                  <span class="onoffswitch-inner"></span>
-                  <span class="onoffswitch-switch"></span>
-                </label>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </section>
+                <label for="inputDate" class="color-turquoise">date</label>
+              </li>
+              <li class="vertical-middle padding-top-bottom">
+                <div class="onoffswitch">
+                  <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked
+                         @click="orderTruthiness" v-model="truthinessOrder">
+                  <label class="onoffswitch-label" for="myonoffswitch">
+                    <span class="onoffswitch-inner"></span>
+                    <span class="onoffswitch-switch"></span>
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
 
 
       <!-- @@@ FACTS CONTAINER @@@ -->
@@ -75,9 +108,46 @@
               <hr class="ellipse">
             </div>
           </div>
-            <div v-else-if="!queryHasBeenSent">
-                First query
+          <div v-else-if="!queryHasBeenSent" class="row">
+            <div class="cell-12">
+              <div class="txt-size-jumbo align-center">
+                <span class="txt-upper color-turquoise APP_font_bebase">what</span>
+                <span class="color-asbestos txt-size-giant APP_font_vincentia">is</span>
+                <span class="">it</span>
+                <span class="color-turquoise">?</span>
+              </div>
+              <p class="margin-50-top">
+                Facts Checker is a project realized in surroundings of the training called DNR2-i at the university of Caen. The aim of this project was to develop a
+                " <span class="txt-b">Hoax Search Engine</span>
+                ". Indeed, with the search bar above, users are able to research several hoax according different keywords from the website
+                <a class="txt-b hover-color-green-sea color-turquoise" title="Link to Politifact.com"
+                   href="http://www.politifact.com/truth-o-meter/statements/">Politifact.com</a>. Each hoax get an authenticity rate and some filters are available in order to sort the result of the research by date and by authneticity rate.
+              </p>
+              <h3 class="margin-50-top">Used Technologies</h3>
+              <ul class="list-style-circle">
+                <li class="padding-top-bottom">
+                  <a class="color-turquoise hover-color-green-sea" href="http://elasticlunr.com/"
+                     title="Link to Elasticlunr.js" target="_blank">Elasticlunr.js</a>
+                </li>
+                <li class="padding-top-bottom">
+                  <a class="color-turquoise hover-color-green-sea" href="https://nodejs.org/en/"
+                     title="Link to Node.js" target="_blank">Node.js</a>
+                </li>
+                <li class="padding-top-bottom">
+                  <a class="color-turquoise hover-color-green-sea" href="https://github.com/"
+                     title="Link to GitHub" target="_blank">GitHub</a>
+                </li>
+                <li class="padding-top-bottom">
+                  <a class="color-turquoise hover-color-green-sea" href="https://www.npmjs.com/"
+                     title="Link to Npm" target="_blank">Npm</a>
+                </li>
+                <li class="padding-top-bottom">
+                  <a class="color-turquoise hover-color-green-sea" href="https://vuejs.org/"
+                     title="Link to Vue.js" target="_blank">Vue.js</a>
+                </li>
+              </ul>
             </div>
+          </div>
           <div v-else v-for="fact in facts"
                class="row-column-reverse-s flex-y-center flex-y-stretch-s flex-x-around margin-50-bottom">
             <div class="cell-8">
@@ -174,17 +244,39 @@
         this.facts = this.truthinessOrder ? filter.orderArrayByTruthinessTrue() : filter.orderArrayByTruthinessFalse()
       }
     },
-    mounted () {}
+    mounted () {
+      window.onload = function () {
+        let acc = document.getElementsByClassName('accordion')
+        let i
+
+        for (i = 0; i < acc.length; i++) {
+          acc[i].onclick = function () {
+            if (this.firstChild.innerHTML.charCodeAt(0) === 9660) {
+              this.firstChild.innerHTML = '\u25B2'
+            } else {
+              this.firstChild.innerHTML = '\u25BC'
+            }
+            this.classList.toggle('active')
+            let panel = this.nextElementSibling
+            if (panel.style.maxHeight) {
+              panel.style.maxHeight = null
+            } else {
+              panel.style.maxHeight = panel.scrollHeight + 'px'
+            }
+          }
+        }
+      }
+    }
   }
 </script>
 <style lang="scss">
-    .loader {
-        position: absolute;
-        margin: auto;
-        top: 50%;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        z-index: 10;
-    }
+  .loader {
+    position: absolute;
+    margin: auto;
+    top: 50%;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+  }
 </style>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import '../css/vendors/normalize.min.css'
-import 'magic-monkey/stylesheets/magic-monkey-1.0.14.min.css'
+import 'magic-monkey/stylesheets/magic-monkey.min.css'
 import '../css/components/checkboxes.min.css'
-import '../css/components/toggleSwitch.css'
+import '../css/components/toggleSwitch.min.css'
+import '../css/components/accordion.min.css'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
